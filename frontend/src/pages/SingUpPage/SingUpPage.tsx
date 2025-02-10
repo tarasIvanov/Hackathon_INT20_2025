bae83dee157c4639281fcc8efd45763fce1cf8d0
Sholudyvyy <stashohulia@gmail.com>
2025-02-10 19:07:05 +0200

    Added avatar for registration. And added possibility to random change this avatar

--- frontend/src/pages/SingUpPage/SingUpPage.tsx
+++ frontend/src/pages/SingUpPage/SingUpPage.tsx
@@ -1,81 +1,86 @@
 /* eslint-disable no-console */
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
-import FormLabel from '@mui/material/FormLabel';
-import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
-import MuiCard from '@mui/material/Card';
-import { styled } from '@mui/material/styles';
-import { GoogleIcon } from '../../components/CustomIcons';
-import { NavLink } from 'react-router';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import MuiCard from "@mui/material/Card";
+import { styled } from "@mui/material/styles";
+import { GoogleIcon } from "../../components/CustomIcons";
+import { NavLink } from "react-router";
+import { Avatar, Badge, IconButton } from "@mui/material";
+import CasinoIcon from "@mui/icons-material/Casino";
+import { createAvatar } from "@dicebear/core";
+import { adventurer } from "@dicebear/collection";
 
 const Card = styled(MuiCard)(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignSelf: 'center',
-  width: '100%',
+  display: "flex",
+  flexDirection: "column",
+  alignSelf: "center",
+  width: "100%",
   padding: theme.spacing(4),
   gap: theme.spacing(2),
-  margin: 'auto',
+  margin: "auto",
   boxShadow:
-    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
-  [theme.breakpoints.up('sm')]: {
-    width: '450px',
+    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  [theme.breakpoints.up("sm")]: {
+    width: "450px",
   },
-  ...theme.applyStyles('dark', {
+  ...theme.applyStyles("dark", {
     boxShadow:
-      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
   }),
 }));
 
 const SignUpContainer = styled(Stack)(() => ({
-  height: 'auto',
+  height: "auto",
 }));
 
 export default function SignUpPage() {
   const [emailError, setEmailError] = React.useState(false);
-  const [emailErrorMessage, setEmailErrorMessage] = React.useState('');
+  const [emailErrorMessage, setEmailErrorMessage] = React.useState("");
   const [passwordError, setPasswordError] = React.useState(false);
-  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState('');
+  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState("");
   const [nameError, setNameError] = React.useState(false);
-  const [nameErrorMessage, setNameErrorMessage] = React.useState('');
+  const [nameErrorMessage, setNameErrorMessage] = React.useState("");
+  const [avatar, setAvatar] = React.useState(newAvatar());
 
   const validateInputs = () => {
-    const email = document.getElementById('email') as HTMLInputElement;
-    const password = document.getElementById('password') as HTMLInputElement;
-    const name = document.getElementById('name') as HTMLInputElement;
+    const email = document.getElementById("email") as HTMLInputElement;
+    const password = document.getElementById("password") as HTMLInputElement;
+    const name = document.getElementById("name") as HTMLInputElement;
 
     let isValid = true;
 
     if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
       setEmailError(true);
-      setEmailErrorMessage('Please enter a valid email address.');
+      setEmailErrorMessage("Please enter a valid email address.");
       isValid = false;
     } else {
       setEmailError(false);
-      setEmailErrorMessage('');
+      setEmailErrorMessage("");
     }
 
     if (!password.value || password.value.length < 6) {
       setPasswordError(true);
-      setPasswordErrorMessage('Password must be at least 6 characters long.');
+      setPasswordErrorMessage("Password must be at least 6 characters long.");
       isValid = false;
     } else {
       setPasswordError(false);
-      setPasswordErrorMessage('');
+      setPasswordErrorMessage("");
     }
 
     if (!name.value || name.value.length < 1) {
       setNameError(true);
-      setNameErrorMessage('Name is required.');
+      setNameErrorMessage("Name is required.");
       isValid = false;
     } else {
       setNameError(false);
-      setNameErrorMessage('');
+      setNameErrorMessage("");
     }
 
     return isValid;
@@ -88,12 +93,19 @@ export default function SignUpPage() {
     }
     const data = new FormData(event.currentTarget);
     console.log({
-      name: data.get('name'),
-      lastName: data.get('lastName'),
-      email: data.get('email'),
-      password: data.get('password'),
+      name: data.get("name"),
+      lastName: data.get("lastName"),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
+
+  function newAvatar() {
+    return createAvatar(adventurer, {
+
+      seed: Math.random().toString(36).substr(2, 9),
+    }).toDataUri();
+  }
 
   return (
     <>
@@ -102,14 +114,56 @@ export default function SignUpPage() {
           <Typography
             component="h1"
             variant="h4"
-            sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+            sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)", textAlign: "center"}}
           >
             Sign up
           </Typography>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Badge
+              overlap="circular"
+              sx={{ bgcolor: "primary.main", borderRadius: "50%" }}
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              badgeContent={
+                <IconButton
+                  onClick={() => setAvatar(newAvatar())}
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    bgcolor: "#F5F5F5",
+                    borderRadius: "50%",
+                    "&:hover": { bgcolor: "#e0e0e0" },
+                    p: 1,
+                  }}
+                >
+                  <CasinoIcon
+                    sx={{
+                      fontSize: 24,
+                      color: "black",
+                    }}
+                  />
+                </IconButton>
+              }
+            >
+              <Avatar
+                alt="Profile avatar"
+                src={avatar}
+                sx={{ width: 120, height: 120, p: 2}}
+              />
+            </Badge>
+          </Box>
+
           <Box
             component="form"
             onSubmit={handleSubmit}
-            sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
+            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
           >
             <FormControl>
               <FormLabel htmlFor="name">Full name</FormLabel>
@@ -122,7 +176,7 @@ export default function SignUpPage() {
                 placeholder="Jon Snow"
                 error={nameError}
                 helperText={nameErrorMessage}
-                color={nameError ? 'error' : 'primary'}
+                color={nameError ? "error" : "primary"}
               />
             </FormControl>
             <FormControl>
@@ -137,7 +191,7 @@ export default function SignUpPage() {
                 variant="outlined"
                 error={emailError}
                 helperText={emailErrorMessage}
-                color={passwordError ? 'error' : 'primary'}
+                color={passwordError ? "error" : "primary"}
               />
             </FormControl>
             <FormControl>
@@ -153,7 +207,7 @@ export default function SignUpPage() {
                 variant="outlined"
                 error={passwordError}
                 helperText={passwordErrorMessage}
-                color={passwordError ? 'error' : 'primary'}
+                color={passwordError ? "error" : "primary"}
               />
             </FormControl>
             <Button
@@ -166,23 +220,19 @@ export default function SignUpPage() {
             </Button>
           </Box>
           <Divider>
-            <Typography sx={{ color: 'text.secondary' }}>or</Typography>
+            <Typography sx={{ color: "text.secondary" }}>or</Typography>
           </Divider>
-          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
             <Button
               fullWidth
               variant="outlined"
-              onClick={() => alert('Sign up with Google')}
+              onClick={() => alert("Sign up with Google")}
               startIcon={<GoogleIcon />}
             >
               Sign up with Google
             </Button>
-            <Typography sx={{ textAlign: 'center' }}>
-              Already have an account?{' '}
-              <NavLink
-               to='/sing-in'>
-                Sign in
-              </NavLink>
+            <Typography sx={{ textAlign: "center" }}>
+              Already have an account? <NavLink to="/sing-in">Sign in</NavLink>
             </Typography>
           </Box>
         </Card>
